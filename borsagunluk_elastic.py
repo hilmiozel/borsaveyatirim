b059d9e74d3c17dfde12f3c6cd9f4504d8c1e0bf
HilmiOzel <hilmi.ozel@gmail.com>
2024-02-09 01:23:24 +0300

    fix

--- borsagunluk_elastic.py
+++ borsagunluk_elastic.py
@@ -66,17 +66,17 @@ for hisse_kodu in tum_hisseler:
                         if columns[0].text.strip() == 'A. Ortalama':
                             a_ortalama=float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip() else 0.0
                 if toplam_islem and toplam_hacim and a_ortalama:
+                    doc1 = {
+                        "timestamp": datetime.now(),
+                        'kod': hisse_kodu,
+                        'gunluk_toplam_islem': toplam_islem,
+                        'gunluk_toplam_hacim': toplam_hacim,
+                        'a_ortalama': a_ortalama
+                    }
+                    
+                    resp = es.index(index="borsa_yapikredi_daily", document=doc1)
                     break
-        doc1 = {
-                "timestamp": datetime.now(),
-                'kod': hisse_kodu,
-                'gunluk_toplam_islem': toplam_islem,
-                'gunluk_toplam_hacim': toplam_hacim,
-                'a_ortalama': a_ortalama
-            }
-            
-        resp = es.index(index="borsa_yapikredi_daily", document=doc1)
-    
+        
     # URL'den sayfa içeriğini getirin
     response = requests.get(URL1)
     html_content = response.text
@@ -113,33 +113,34 @@ for hisse_kodu in tum_hisseler:
             columns = row.find_all('td')
             kod = columns[0].text.strip()  # Kod sütunu
             year = 2022 + int(row.attrs['class'][1])
-            fk = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().isdigit() else 0.0
-            fdfavök = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().isdigit() else 0.0
-            fdsatis = float(columns[3].text.strip().replace('.', '').replace(',', '.')) if columns[3].text.strip().isdigit() else 0.0
-            pddd = float(columns[4].text.strip().replace('.', '').replace(',', '.')) if columns[4].text.strip().isdigit() else 0.0
+            fk = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            fdfavök = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            fdsatis = float(columns[3].text.strip().replace('.', '').replace(',', '.')) if columns[3].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            pddd = float(columns[4].text.strip().replace('.', '').replace(',', '.')) if columns[4].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
             result = columns[5].text.strip()
             
-        doc2 = {
-                "timestamp": datetime.now(),
-                'kod': hisse_kodu,
-                'yil': year,
-                'fk': fk,
-                'fdfavök': fdfavök,
-                'fdsatis': fdsatis,
-                'pddd': pddd,
-                'raportipi': result
-            }
+            doc2 = {
+                    "timestamp": datetime.now(),
+                    'kod': hisse_kodu,
+                    'yil': year,
+                    'fk': fk,
+                    'fdfavök': fdfavök,
+                    'fdsatis': fdsatis,
+                    'pddd': pddd,
+                    'raportipi': result
+                }
+                
+            resp = es.index(index="borsa_is_finansaloranlartahmin", document=doc2)
             
-        resp = es.index(index="borsa_is_finansaloranlartahmin", document=doc2)
-        
+    doc3=''
     if yurtdisiiskonto_table:
         rows = yurtdisiiskonto_table.find_all('tr')
         for row in rows[2:]:  # İlk satır başlıkları içerdiğinden atlanır
             columns = row.find_all('td')
             kod = columns[0].text.strip()  # Kod sütunu
-            son_fiyat = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().isdigit() else 0.0
-            fk = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().isdigit() else 0.0
-            yurtdisi_fk_prim_oran = float(columns[3].text.strip().replace('.', '').replace(',', '.')) if columns[3].text.strip().isdigit() else 0.0
+            son_fiyat = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            fk = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            yurtdisi_fk_prim_oran = float(columns[3].text.strip().replace('.', '').replace(',', '.')) if columns[3].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
             # fdfavök = float(columns[4].text.strip().replace('.', '').replace(',', '.')) if columns[4].text.strip() else 0.0
             # fdfavök_prim_oran = float(columns[5].text.strip().replace('.', '').replace(',', '.')) if columns[5].text.strip() else 0.0
             # pddd = float(columns[6].text.strip().replace('.', '').replace(',', '.')) if columns[6].text.strip() else 0.0
@@ -150,8 +151,8 @@ for hisse_kodu in tum_hisseler:
         for row in rows[2:]:  # İlk satır başlıkları içerdiğinden atlanır
             columns = row.find_all('td')
             kod = columns[0].text.strip()  # Kod sütunu
-            fk = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().isdigit() else 0.0
-            tarihsel_fk_prim_oran = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().isdigit() else 0.0
+            fk = float(columns[1].text.strip().replace('.', '').replace(',', '.')) if columns[1].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
+            tarihsel_fk_prim_oran = float(columns[2].text.strip().replace('.', '').replace(',', '.')) if columns[2].text.strip().startswith(('-','0', '1', '2', '3', '4', '5', '6', '7', '8', '9')) else 0.0
             # fdfavök = float(columns[3].text.strip().replace('.', '').replace(',', '.')) if columns[3].text.strip() else 0.0
             # fdfavök_prim_oran = float(columns[4].text.strip().replace('.', '').replace(',', '.')) if columns[4].text.strip() else 0.0
             # pddd = float(columns[5].text.strip().replace('.', '').replace(',', '.')) if columns[5].text.strip() else 0.0
@@ -168,7 +169,6 @@ for hisse_kodu in tum_hisseler:
                 'tarihsel_fk_prim_oran': tarihsel_fk_prim_oran
             }
             
-        resp = es.index(index="borsa_is_iskontolar", document=doc3)
     
     elif tarihselortalamalar_table:
         
@@ -179,7 +179,6 @@ for hisse_kodu in tum_hisseler:
                 'tarihsel_fk_prim_oran': tarihsel_fk_prim_oran
             }
             
-        resp = es.index(index="borsa_is_iskontolar", document=doc3)
         
     elif yurtdisiiskonto_table:
         
@@ -191,5 +190,7 @@ for hisse_kodu in tum_hisseler:
                 'yurtdisi_fk_prim_oran': yurtdisi_fk_prim_oran
             }
             
+    
+    if doc3:
         resp = es.index(index="borsa_is_iskontolar", document=doc3)
     
